0115d231a365699f963f0090e3cb9344a045998a
Marcos Soto <you@example.com>
2024-05-15 23:30:31 -0600

    st1 m6 t1

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,38 @@
 package com.epam.mjc.io;
 
 import java.io.File;
-
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class FileReader {
 
-    public Profile getDataFromFile(File file) {
-        return new Profile();
-    }
-}
+	public Profile getDataFromFile(File file) {
+		return new Profile();
+	}
+
+	// Leer el file
+
+	String directory = System.getProperty("user.home");
+	String fileName = "Profile.txt";
+	String absolutePath = directory + File.separator + fileName;{
+
+	try(
+	FileInputStream fileInputStream = new FileInputStream(absolutePath))
+	{
+		int ch;
+
+		while ((ch = fileInputStream.read()) != -1) {
+			System.out.print((char) ch);
+		}
+
+	}catch(
+	FileNotFoundException e)
+	{
+
+	}catch(
+	IOException eg)
+	{
+
+	}
+}}
